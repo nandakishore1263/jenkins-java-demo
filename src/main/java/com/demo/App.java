1c9c08576fc4c5a27729b9f4f8dad5b68d03ec41
nandakishore1263 <nandakishore.g1263@gmail.com>
2026-06-25 12:16:34 +0530

    Create App.java

---
+++ src/main/java/com/demo/App.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins Pipeline!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
